96c112773763ed9e2131f72a983aae295f1dbc6b
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2021-07-24 23:42:41 +0300

    Fix bug where getting the memory map causes freeze

--- Source/main.zig
+++ Source/main.zig
@@ -42,12 +42,12 @@ pub fn main() void {
     _ = system_table.con_out.?.outputString(w("Welcome!\n\r"));
 
     // Open ESP
-    var image: ?*uefi.protocols.LoadedImageProtocol = undefined;
+    var image: *uefi.protocols.LoadedImageProtocol = undefined;
     assert(boot_services.handleProtocol(uefi.handle, &uefi.protocols.LoadedImageProtocol.guid, @ptrCast(*?*c_void, &image)), "Unable to open image info.", @src());
-    var fs: ?*uefi.protocols.SimpleFileSystemProtocol = undefined;
-    assert(boot_services.handleProtocol(image.?.device_handle.?, &uefi.protocols.SimpleFileSystemProtocol.guid, @ptrCast(*?*c_void, &fs)), "Unable to open filesystem", @src());
-    var esp: ?*uefi.protocols.FileProtocol = undefined;
-    assert(fs.?.openVolume(&esp.?), "Unable to open volume.", @src());
+    var fs: *uefi.protocols.SimpleFileSystemProtocol = undefined;
+    assert(boot_services.handleProtocol(image.device_handle.?, &uefi.protocols.SimpleFileSystemProtocol.guid, @ptrCast(*?*c_void, &fs)), "Unable to open filesystem", @src());
+    var esp: *uefi.protocols.FileProtocol = undefined;
+    assert(fs.openVolume(&esp), "Unable to open volume.", @src());
 
     // Find ACPI pointer
     if (findCfgTable(uefi.tables.ConfigurationTable.acpi_20_table_guid)) |_| {
@@ -56,19 +56,18 @@ pub fn main() void {
         _ = system_table.con_out.?.outputString(w("Found ACPI v1.\n\r"));
     } else |_| @panic("No ACPI found.");
 
+    // Get memory map and exit boot services
     _ = system_table.con_out.?.outputString(w("Exiting boot services..."));
-    var mem_map_size: usize = undefined;
+    var mem_map_size: usize = 0;
     var mem_map: [*]uefi.tables.MemoryDescriptor = undefined;
     var map_key: usize = undefined;
     var desc_size: usize = undefined;
     var desc_ver: u32 = undefined;
-    // Get memory map size
     _ = boot_services.getMemoryMap(&mem_map_size, mem_map, &map_key, &desc_size, &desc_ver);
-    // Add new descriptor
     mem_map_size += desc_size;
     assert(boot_services.allocatePool(uefi.tables.MemoryType.BootServicesData, mem_map_size, @ptrCast(*[*]align(8) u8, &mem_map)), "Unable to allocate memory map.", @src());
     assert(boot_services.getMemoryMap(&mem_map_size, mem_map, &map_key, &desc_size, &desc_ver), "Unable to get memory map.", @src());
-    // Exit boot services
+
     assert(boot_services.exitBootServices(uefi.handle, map_key), "Unable to exit boot services.", @src());
 
     // Jump to kernel code
