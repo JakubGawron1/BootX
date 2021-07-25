d989895f6e67198cbac890e2473ae316f464b625
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2021-07-25 21:34:01 +0300

    Rename "Source" folder to "src"

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const Builder = @import("std").build.Builder;
 const Target = @import("std").Target;
 
 pub fn build(builder: *Builder) void {
-    const fwlauncher = builder.addExecutable("BOOTX64", "Source/main.zig");
+    const fwlauncher = builder.addExecutable("BOOTX64", "src/main.zig");
     fwlauncher.setBuildMode(builder.standardReleaseOptions());
     fwlauncher.setTarget(.{
         .cpu_arch = Target.Cpu.Arch.x86_64,
@@ -12,7 +12,7 @@ pub fn build(builder: *Builder) void {
     fwlauncher.setOutputDir("Build/Drive/EFI/BOOT");
     fwlauncher.addPackage(.{
         .name = "helpers",
-        .path = .{ .path = "Source/helpers/helpers.zig" },
+        .path = .{ .path = "src/helpers/helpers.zig" },
     });
     builder.default_step.dependOn(&fwlauncher.step);
     builder.installArtifact(fwlauncher);
