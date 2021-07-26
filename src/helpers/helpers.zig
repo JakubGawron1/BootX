1ed82bbc023573015af0a433e29b749af84abfcc
Visual <30368284+VisualDevelopment@users.noreply.github.com>
2021-07-26 21:09:52 +0300

    Use many-item pointer slice for findCfgTable

--- src/helpers/helpers.zig
+++ src/helpers/helpers.zig
@@ -4,12 +4,8 @@ pub const panic = @import("panic.zig").panic;
 const uefi = @import("std").os.uefi;
 
 pub fn findCfgTable(guid: uefi.Guid) !uefi.tables.ConfigurationTable {
-    var i: usize = 0;
-    while (i < uefi.system_table.number_of_table_entries) : (i += 1) {
-        const table = uefi.system_table.configuration_table[i];
-
+    for (uefi.system_table.configuration_table[0..uefi.system_table.number_of_table_entries]) |table|
         if (guid.eql(table.vendor_guid)) return table;
-    }
 
     return error.CfgTableNotFound;
 }
